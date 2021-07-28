b9473ab64a8ec98b6cdc0cb40e938ec0b820457d
Rosalba Fuentes <84886793+Rosalbafs@users.noreply.github.com>
2021-07-27 22:02:24 -0500

    Add files via upload
     3 files changed, 192 insertions(+), 147 deletions(-)

--- funcional.js
+++ funcional.js
@@ -1,54 +1,114 @@
 class Product {
-    name = '';
-    price = 0;
-    year = 0;
-     constructor(name, price, year) {
-      this.name = name
-      this.price = price
-      this.year = year
-      }
-
-      registrar() {
-        return `
-        Product ${this.name}, 
-        price ${this.price}, 
-        year ${this.year} 
-        `;
-      }
-
-      get info() {
-        return this.name
-      }
-    }
-let product1 = new Product(
-    'rice',
-    1.15,
-    2018
-  )
-
-let product2 = new Product(
+  name = '';
+  price = 0;
+  year = 0;
+  constructor(name, price, year) {
+  this.name = name
+  this.price = price
+  this.year = year
+  }
+  
+  registrar() {
+  return `
+  Product ${this.name}, 
+  price ${this.price}, 
+  year ${this.year} 
+  `;
+  }
+  
+  get info() {
+  return this.name
+  }
+ }
+ let product1 = new Product(
+  'rice',
+  1.15,
+  2018
+ )
+  
+ let product2 = new Product(
   'pepper',
   2,
   2019
-)
-
-let product3 = new Product(
+ )
+  
+ let product3 = new Product(
   'bean',
   3.2,
   2020
-)
-
-function addProduct(name, price, year)
-{let product = new Product(name, price, year)
-  console.log(product.registrar())
-}
-
-document.getElementById('save').addEventListener('click',function(){
-let nameValue = document.getElementById('name').value
-let priceValue = document.getElementById('price').value
-let yearValue= document.getElementById('year').value
-addProduct(namevalue, pricevalue, yearvalue)
-})
-
-
+ )
+ let productList = []
+  
+ function addProduct(name, price, year) {
+  let product = new Product(name, price, year)
+  productList.push(product)
+  insertProductHtml()
+  clearInputs()
+ }
+ function clearInputs() {
+  document.getElementById('name').value = ''
+  document.getElementById('price').value = ''
+  document.getElementById('year').value = ''
+ }
+  
+ document.getElementById('save').addEventListener('click', function () {
+  let nameValue = document.getElementById('name').value
+  let priceValue = document.getElementById('price').value
+  let yearValue = document.getElementById('year').value
+  addProduct(nameValue, priceValue, yearValue)
+ })
+  
+ function insertProductHtml() {
+  productList.forEach(function (product, index) {
+  let html = `
+  <tr id="${index}">
+  <th id="name">${product.name}</th>
+  <th id="price">${product.price}</th>
+  <th id="year">${product.year}</th>
+  <th>
+  <button class="btn-info btnEdit">Edit</button>
+  <button class="btn-danger btnDelete">Delete</button>
+  </th>
+  </tr>`
+  document.getElementById('product-list').innerHTML += html
+  })
+ }
+  
+ // tener una lista de productos utilizar foreach
+ /* document.getElementById('table-content').innerHtml += `
+  <tr>
+  <th id="name">name</th>
+  <th id="price">price</th>
+  <th id="year">year</th>
+  <th>
+  <button class="btn-info btnEdit">Edit</button>
+  <button class="btn-danger btnDelete">Delete</button>
+  </th>
+  </tr>`
+  */
+  
+ // 1. mostrar en pantalla todos los productos
+ // 2. crear html con los datos del producto
+ /*
+  <tr>
+  <th id="name">name</th>
+  <th id="price">price</th>
+  <th id="year">year</th>
+  <th>
+  <button class="btn-info btnEdit">Edit</button>
+  <button class="btn-danger btnDelete">Delete</button>
+  </th>
+  </tr>
+ */
+ // 3. agregar producto a la lista array[] (metodo push en array)
+ /* [
+  {
+  name: 'rice',
+  price: 1.15,
+  year: 2018
+  }
+ ] */
+  
+ // 4. borrar producto de la lista array[] (metodo splice en array)
+ // 5. actualizar producto al darle click a edit y actualizar los datos de la lista
 
